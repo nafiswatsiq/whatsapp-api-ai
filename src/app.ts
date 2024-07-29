746659d04832a00974f8311cbc4be0fd5eb7886f
nafiswatsiq <amrullohnafis@gmail.com>
2024-07-29 23:06:37 +0700

    refactor: rm app

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from 'express'
+import express, { Request, Response } from 'express'
 import { Initialize } from './sockets/whatsappSocket'
 import bodyParser from 'body-parser'
 
