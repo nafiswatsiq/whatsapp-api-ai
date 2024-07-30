c2982d1a9923c71a60fe153f6aa05ccfc641db76
nafiswatsiq <amrullohnafis@gmail.com>
2024-07-30 18:10:39 +0700

    chore: test

--- dist/server.js
+++ dist/server.js
@@ -10,4 +10,4 @@ const port = process.env.PORT || 8080;
 app_1.default.listen(port, () => {
     console.log(`Server running in http://localhost:${port}`);
 });
-//# sourceMappingURL=index.js.map
+//# sourceMappingURL=server.js.map
